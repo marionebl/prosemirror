a554ce32fc3ec0852b164c94f298a8df6412db4a
Jesús Quintana <jquintana@atlassian.com>
2019-11-10 18:58:21 +1100

    Remove temporal package

--- packages/eslint-plugin-custom/index.js
+++
@@ -1,7 +0,0 @@
-const allRules = {
-  'jsx-uses-vars': require('eslint-plugin-react/lib/rules/jsx-uses-vars'),
-  'jsx-uses-react': require('eslint-plugin-react/lib/rules/jsx-uses-react'),
-};
-module.exports = {
-  rules: allRules,
-};
