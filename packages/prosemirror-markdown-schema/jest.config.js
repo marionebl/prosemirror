beb38a084523ddbb591e68b8d35f4d9da863c2b1
Jesús Quintana <jrqb182@gmail.com>
2019-12-01 12:45:50 +1100

    chore(jest): move default configuration to the top level

--- packages/prosemirror-markdown-schema/jest.config.js
+++ packages/prosemirror-markdown-schema/jest.config.js
@@ -1,19 +1,10 @@
+const createJestConfig = require('../../createJestConfig');
 const packageName = require('./package.json')
   .name.split('@marduke182/')
   .pop();
 
 module.exports = {
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      extends: './babel.config.js',
-    },
-  },
-  testMatch: ['**/__tests__/*.+(ts|tsx|js)'],
-  modulePaths: [`<rootDir>/src/`],
-  moduleDirectories: ['node_modules'],
+  ...createJestConfig(),
   name: packageName,
   displayName: packageName,
   rootDir: './',
